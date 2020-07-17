b36b9bee5d2b39686fd2fe31a61b5a508dac5c12
adewoleopeyemi <58083371+adewoleopeyemi@users.noreply.github.com>
2020-07-17 23:22:19 +0100

    Add files via upload

--- cell.py
+++ cell.py
@@ -1,14 +1,15 @@
 class Cell:
- def __init__(self,cell):
- 	self.deadCell = cell
- 	self.liveCell = cell
+ def __init__(self, cell_state):
+ 	self.cell_state = cell_state
  
- def SetCell(self):
- 	if i == deadCell :
- 		i=0
- 	elif i == liveCell :
- 		i=1
+ def SetCell(self, i):
+ 	if i == 'deadcell' :
+ 		self.cell_state=0
+ 	elif i == 'livecell':
+ 		self.cell_state=1
  	else:
  		raise RuntimeError("Unknown cell status ") 
  	
+ 	def isAlive(self):
+ 	  return self.cell_state
  	
